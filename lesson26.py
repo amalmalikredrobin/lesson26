09b2442515f31556b2c3203917bef6e922f9140c
amalmalik <amalmalik@mail.ru>
2022-01-11 21:11:09 +0500

    last commit

--- lesson26.py
+++ lesson26.py
@@ -1,1 +1,38 @@
-a
+
+
+# [1, 2. True, 'Bye']
+
+# a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# b = []
+# for num in a:
+# a = ['a', 'b', 'c', 'd']
+# b = []
+
+# for i in range(len(a) - 1, -1, -1):
+#     b.append(a[i])
+# print(b)
+
+# a = ['a', 'b', 'c', 'd']
+# num = []
+# l = []
+
+# for i in range(len(a)):
+#     num.append(i * a[i])
+
+# print(num)
+
+# a = ['a', 'b', 'c', 'd']
+# b = []
+
+# for i, letter in enumerate(a, 1):
+#     b.append(letter * i)
+# print(b)
+
+a = ['a', 'b', 'c', 'd']
+b = []
+for  i, ltr in enumerate(a):
+    if i % 2:
+        b.append(ltr.upper())
+    else:
+        b.append(ltr.lower())
+print(b)
